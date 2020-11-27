f1b0427f36fc802ed16d03c0420e302198308056
a.timoshin <killfreedom@yandex.ru>
2020-11-28 02:29:10 +0300

    add personal req and team req

--- src/panels/my_reqts/index.js
+++ src/panels/my_reqts/index.js
@@ -10,9 +10,10 @@ import Button from "@vkontakte/vkui/dist/components/Button/Button";
 import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
 import FormLayoutGroup from "@vkontakte/vkui/dist/components/FormLayoutGroup/FormLayoutGroup";
 import Input from "@vkontakte/vkui/dist/components/Input/Input";
-import Select from "@vkontakte/vkui/dist/components/Select/Select";
 import Link from "@vkontakte/vkui/dist/components/Link/Link";
-
+import Icon24GameOutline from '@vkontakte/icons/dist/24/game_outline';
+import {SelectMimicry} from "@vkontakte/vkui";
+import Select from "@vkontakte/vkui/dist/components/Select/Select";
 const AddReq = ({id}) => {
     const dispatch = useDispatch()
     const ui = useSelector(s => s.dynamic_ui)
@@ -33,7 +34,7 @@ const AddReq = ({id}) => {
                     }}
                     selected={ui.history[ui.history.length - 1].split("/")[1] === 'news'}
                     >
-                    Новости
+                    Поиск игрока
                 </TabsItem>
                 <TabsItem
                     onClick={() => {
@@ -41,12 +42,12 @@ const AddReq = ({id}) => {
                     }}
                     selected={ui.history[ui.history.length - 1].split("/")[1] === 'recomendations'}
                 >
-                    Интересное
+                    Поиск в команду
                 </TabsItem>
             </Tabs>
             </FixedLayout>
 
-            <View style={{marginTop: '0'}} activePanel={ui.history[ui.history.length - 1].split("/")[1]}>
+            <View style={{marginTop: '30px'}} activePanel={ui.history[ui.history.length - 1].split("/")[1]}>
                 <Panel id={"news"}>
                     <Div>
                         <FormLayout>
@@ -57,7 +58,7 @@ const AddReq = ({id}) => {
 
                         <FormLayout>
                             <FormLayoutGroup top="Игра">
-                                <Input type="text" defaultValue="" placeholder="Выберите игру" disabled />
+                                <Input type="text" defaultValue="" placeholder="Выберите игру" disabled  right={<Icon24GameOutline/>}/>
                             </FormLayoutGroup>
                         </FormLayout>
 
@@ -65,14 +66,41 @@ const AddReq = ({id}) => {
                             <Checkbox>Я прочитал и согласен с <Link>правилами</Link></Checkbox>
                         </FormLayout>
                         {/*тут твой уникальный код*/}
-                        <Div style={{ marginTop: 244 }}>
+                        <Div style={{ marginTop: 340 }}>
                             <Button size="xl" stretched style={{ marginRight: 8 }} onClick={() => dispatch(uiActions.back('add_team'))}>Добавить команду</Button>
                         </Div>
                     </Div>
                 </Panel>
                 <Panel id={"recomendations"}>
                     <Div>
-                        Заявки команды
+                        <Div>
+                            <FormLayout>
+                                <FormLayoutGroup top="Текст заявки">
+                                    <Input type="text" defaultValue="" placeholder="Введите текст заявки" />
+                                </FormLayoutGroup>
+                            </FormLayout>
+
+                            <FormLayout>
+                                <FormLayoutGroup top="Игра">
+                                    <Input type="text" defaultValue="" placeholder="Выберите игру" disabled  right={<Icon24GameOutline/>}/>
+                                </FormLayoutGroup>
+                            </FormLayout>
+
+                            <FormLayout>
+                                <Select top="Команда" placeholder="Выберите команду">
+                                    <option value="m">Команда-1</option>
+                                    <option value="f">Команда-2</option>
+                                </Select>
+                            </FormLayout>
+
+                            <FormLayout>
+                                <Checkbox>Я прочитал и согласен с <Link>правилами</Link></Checkbox>
+                            </FormLayout>
+                            {/*тут твой уникальный код*/}
+                            <Div style={{ marginTop: 230 }}>
+                                <Button size="xl" stretched style={{ marginRight: 8 }} onClick={() => dispatch(uiActions.back('add_team'))}>Добавить команду</Button>
+                            </Div>
+                        </Div>
                     </Div>
                 </Panel>
             </View>
