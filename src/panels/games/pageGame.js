c3a1b85bf5ceb57600bf6175d2e425f1d57438a9
Lagumir <Legals92@mail.ru>
2020-11-28 12:01:54 +0300

    cem

--- src/panels/games/pageGame.js
+++ src/panels/games/pageGame.js
@@ -15,7 +15,9 @@ import {
     SimpleCell,
     Avatar,
     FormLayout,
-    Select
+    Select,
+    Cell,
+    Header
 } from '@vkontakte/vkui'
 import Icon28ArrowLeftOutline from '@vkontakte/icons/dist/28/arrow_left_outline';
 import * as uiActions from '../../store/dynamicui/actions'
@@ -42,11 +44,38 @@ const PageGame = ({id}) =>{
         <SimpleCell
             before={<Avatar src={('/ex/avatar.jpg')} size={48}/>}
             text="Игра"
+            description="Steam"
+
         >
             Название игры
         </SimpleCell>
     </List>
     </Group>
+        <Group header={<Header mode="secondary">Статистика</Header>}>
+             <List>
+               <Cell expandable indicator="4 часа">Всего в игре</Cell>
+               <Cell expandable indicator="2 часа">В игре за 2 недели</Cell>
+               <Cell expandable indicator="2.34">У/С</Cell>
+               <Cell expandable indicator="22.1%">Точность</Cell>
+               <Cell expandable indicator="22.36%">В голову</Cell>
+               <Cell expandable indicator="21%">УMVP за матчС</Cell>
+               <Cell expandable indicator="10%">Удачных пистолетных раудовС</Cell>
+             </List>
+        </Group>
+        <FixedLayout vertical="bottom">
+            <Button
+                stretched
+                size="xl"
+                style={{width: '95%', margin: '0 auto'}}
+            >Умный поиск</Button>
+            <Button
+                stretched
+                mode="secondary"
+                size="xl"
+                style={{width: '95%', margin: '0 auto', marginTop:'8px'}}
+                onClick = {() => dispatch(uiActions.push_route('add_req'))}
+            >Создать заявку</Button>
+        </FixedLayout>
     </Panel>
   )
 }
